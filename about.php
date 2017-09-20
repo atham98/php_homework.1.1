a3306936f0d723d07f3e8b3565b6f8ab87fb27d8
Adham Nasrullaev <1998atham@gmail.com>
2017-09-20 20:21:13 +0300

    Add about.php

---
+++ about.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		dl {
+			display: table-row;
+		}
+		dt,dd {
+			display: table-cell;
+			padding: 5px 10px;
+		}
+	</style>
+</head>
+<body>
+		<h1>Страница пользователя Дима</h1>
+	<dl>
+		<dt>Имя</dt>
+		<dd>Адхам</dd>
+	</dl>
+	<dl>
+		<dt>Возраст</dt>
+		<dd>18</dd>
+	</dl>
+	<dl>
+		<dt>Электронная почта</dt>
+		<dd><a href="mailto:1998atham@gmail.com">1998atham@gmail.com</a></dd>
+	</dl>
+	<dl>
+		<dt>Город</dt>
+		<dd>Нефтеюганск</dd>
+	</dl>
+	<dl>
+		<dt>О себе</dt>
+		<dd>Frontend разработчик</dd>
+	</dl>
+</body>
+</html>
